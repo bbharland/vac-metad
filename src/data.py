12f7ebe1c3d1c118523b54b96f7f7bbdc55b0afe
Ben Harland <bbharland@gmail.com>
2026-06-12 16:35:41 -0400

    claude code upgrade data.py

--- src/data.py
+++ src/data.py
@@ -5,18 +5,10 @@ import mdtraj as md
 
 from .DataHandles import DataHandles
 from .param import SimulationParameters
-from .hist2d import histogram2d
-from .opes import bias_potential
-from .force import force_module
 from .calcs import (
     feature_grid_over_dihedrals,
     psi_grid_from_feature_grid,
     cvs_grid_from_feature_grid,
-    calc_kde_grid,
-    free_energy_surface,
-    calc_bias_grid,
-    calc_bias_grid_fm,
-    calc_fes_from_bias
 )
 
 
@@ -28,7 +20,7 @@ def simulation_data(p, subdir=None, assign_labels=None, loud=True, num_points=10
             - subdir        subdir is a string
             - step_n        subdir is an integer, n
 
-    Parameters:
+    Parameters
     ----------
     p : SimulationParameters => basedir = Path(p.working_dir)
                         Path => basedir = p
@@ -47,13 +39,6 @@ def simulation_data(p, subdir=None, assign_labels=None, loud=True, num_points=10
 
     num_points : int => sd.theta_grid has num_points points
                 None => sd.theta_grid not assigned
-
-    TODO: the lagtime and lagframes should come from the sim parameters object, no?
-
-    TODO: in 'loud' section, don't check both dihedrals and features this way.
-    Better to check that they are either both there or neither.  Complain if there is just one.
-
-    TODO:  comment on the fact that 'theta_grid' get assigned no matter what if it is found not to exist
     """
     lagframes = 1
 
@@ -67,45 +52,59 @@ def simulation_data(p, subdir=None, assign_labels=None, loud=True, num_points=10
         basedir = Path(p)
         lagtime = 1
     else:
-        raise TypeError(f'Type {type(p)} not known.')
+        raise TypeError(f"Type {type(p)} not known.")
 
     if subdir is None:
         working_dir = basedir
     elif type(subdir) is int:
-        working_dir = basedir / f'step_{subdir}'
+        working_dir = basedir / f"step_{subdir}"
     elif type(subdir) is str:
         working_dir = basedir / subdir
     else:
         raise TypeError(f"Can't handle 'subdir' type {type(subdir)}")
 
     if not working_dir.exists():
-        print(f'Creating new directory {working_dir}')
+        print(f"Creating new directory {working_dir}")
         working_dir.mkdir()
 
     sd = SimulationData(working_dir, lagtime, lagframes, assign_labels=assign_labels)
 
     # Always assign 'theta_grid' (unless num_point is explicitly set to None).  This makes code easier to reason about
     if num_points is not None:
-        if not sd.files['theta_grid'].exists():
+        if not sd.files["theta_grid"].exists():
             sd.save_theta_grid(num_points=num_points)
         else:
-            sd.assign_objects(['theta_grid'])
+            sd.assign_objects(["theta_grid"])
 
     if loud:
-        if sd.files['features'].exists():
-            if hasattr(sd, 'features') and sd.features is not None:
-                print(f'Loaded SimulationData object from {working_dir} with {len(sd.features)} frames')
-            elif hasattr(sd, 'dihedrals') and sd.dihedrals is not None:
-                print(f'Loaded SimulationData object from {working_dir} with {len(sd.dihedrals)} frames')
-            else:
-                print("Simulation exists, but not setting 'features' or 'features' attributes")
-        else:
-            print(f'Creating new SimulationData object with no data at {working_dir}')
+        dihedrals_exist = sd.files["dihedrals"].exists()
+        features_exist = sd.files["features"].exists()
 
-    # TODO: UNCOMMENT THIS!
-    # # make sure SRV net is on correct device
-    # if sd.files['srv'].exists() and hasattr(sd, 'srv'):
-    #     sd.srv.net.to(device=sd.srv.device)
+        if dihedrals_exist and features_exist:
+            if hasattr(sd, "features") and sd.features is not None:
+                print(
+                    f"Loaded SimulationData object from {working_dir} "
+                    f"with {len(sd.features)} frames"
+                )
+            elif hasattr(sd, "dihedrals") and sd.dihedrals is not None:
+                print(
+                    f"Loaded SimulationData object from {working_dir} "
+                    f"with {len(sd.dihedrals)} frames"
+                )
+            else:
+                print(f"Loaded SimulationData object from {working_dir}")
+        elif not dihedrals_exist and not features_exist:
+            print(f"Creating new SimulationData object with no data at {working_dir}")
+        else:
+            missing = "dihedrals" if features_exist else "features"
+            print(
+                f"WARNING: {working_dir} has one of dihedrals/features but "
+                f"not the other (missing {missing})"
+            )
+
+    # make sure SRV net is on correct device
+    if sd.files["srv"].exists() and hasattr(sd, "srv"):
+        sd.srv.net.to(device=sd.srv.device)
 
     return sd
 
@@ -126,37 +125,60 @@ class SimulationData(DataHandles):
             sd.save_eigen_data(srv)
 
         4. Project trajectory eigenfunctions/CVs onto dihedral grid
-            sd.save_kde_data(kde, kde_c, s1, s2)
+            sd.save_grid_data(srv)
             sd.save_and_assign_objects({'bias_shift': bias_shift})
+
+    KDE/FES/bias-grid data (formerly save_kde_fes_bias_data) has moved to
+    data_attic.py pending the grid-calculations redesign -- see claude.txt.
     """
+
     data_filenames = [
-        #---------------------- numpy arrays -------------------------
-        'widths.npy',
+        # ---------------------- numpy arrays -------------------------
+        "widths.npy",
         #                       current step data
-        'dihedrals.npy',        'features.npy',      'weights.npy',
-        'biases.npy',           'eigvals.npy',       'timescales.npy',
-        'bias_shift.npy',
+        "dihedrals.npy",
+        "features.npy",
+        "heavy_atoms.npy",
+        "weights.npy",
+        "biases.npy",
+        "eigvals.npy",
+        "timescales.npy",
+        "bias_shift.npy",
         #                       full dataset
-        'psi.npy',              'cvs.npy',
+        "psi.npy",
+        "cvs.npy",
         #                       grids
-        's1.npy',               's2.npy',            'grid.pickle',
-        'bias.pickle',          'fes.pickle',        'theta_grid.npy',
-        'feature_grid.npy',     'psi_grid.npy',      'cvs_grid.npy',
+        "s1.npy",
+        "s2.npy",
+        "grid.pickle",
+        "bias.pickle",
+        "fes.pickle",
+        "theta_grid.npy",
+        "feature_grid.npy",
+        "psi_grid.npy",
+        "cvs_grid.npy",
         #                       MSM states
-        'states.npy',           'states_core.npy',
+        "states.npy",
+        "states_core.npy",
         #                       converging distributions
         # 'num_frames_conv.npy',  'pgrids_conv.npy',
-        #---------------------- pytorch models -----------------------
+        # ---------------------- pytorch models -----------------------
         # 'force_module.pt' needs to be opened with 'torch.jit.load'
-        # 'srv_net.pt',    # TODO: NEED TO UNCOMMENT THIS LINE!
-        #---------------------- objects ------------------------------
-        # 'vampnet.pickle',       'srv.pickle', # TODO: UNCOMMENT THIS LINE!
-        'kde.pickle',
-        'kde_c.pickle',         'final_positions.pickle',
-        'metad.pickle',         'metad_kde.pickle'
+        "srv_net.pt",
+        # ---------------------- objects ------------------------------
+        "vampnet.pickle",
+        "srv.pickle",
+        "kde.pickle",
+        "kde_c.pickle",
+        "final_positions.pickle",
+        "metad.pickle",
+        "metad_kde.pickle",
     ]
-    other_filenames = {'h5file': 'traj.h5', 'dcdfile': 'traj.dcd',
-                       'outfile': 'traj.out'}
+    other_filenames = {
+        "h5file": "traj.h5",
+        "dcdfile": "traj.dcd",
+        "outfile": "traj.out",
+    }
 
     def __init__(self, working_dir, lagtime, lagframes, assign_labels=None):
         """Parameters
@@ -171,7 +193,6 @@ class SimulationData(DataHandles):
             If None: assign all objects in SimulationData.data_filenames
             Else: assign all obects in list of labels
         """
-        # print(f'{type(self)} init: {assign_labels = }')
         super().__init__(working_dir, assign_labels=assign_labels)
         self.lagtime = lagtime
         self.lagframes = lagframes
@@ -182,7 +203,7 @@ class SimulationData(DataHandles):
         Note that 'theta_grid' is assigned always (when 'simlation_data' function is used for construction)!
         """
         theta_grid = np.linspace(-np.pi, np.pi, num_points)
-        self.save_and_assign_objects({'theta_grid': theta_grid})
+        self.save_and_assign_objects({"theta_grid": theta_grid})
 
     def save_feature_data(self, periodic=True, recalculate=False, pdbfile=None):
         """Save files if they don't exist (or recalculate is set to True):
@@ -197,201 +218,96 @@ class SimulationData(DataHandles):
         pdbfile : str
             If anything is provided here, prefer using dcd file.  Otherwise h5 file will be used.
         """
-        if pdbfile and self.files['dcdfile'].exists():
-            traj = md.load_dcd(self.files['dcdfile'], top=pdbfile)
-        elif self.files['h5file'].exists():
-            traj = md.load(self.files['h5file'])
+        if pdbfile and self.files["dcdfile"].exists():
+            traj = md.load_dcd(self.files["dcdfile"], top=pdbfile)
+        elif self.files["h5file"].exists():
+            traj = md.load(self.files["h5file"])
         else:
-            raise FileNotFoundError('Need either h5 file or dcd + pdb file')
+            raise FileNotFoundError("Need either h5 file or dcd + pdb file")
 
-        file = self.files['dihedrals']
+        file = self.files["dihedrals"]
         if not file.exists() or recalculate:
-            print('writing', file)
-            dihedrals = np.hstack([md.compute_phi(traj)[1],
-                                   md.compute_psi(traj)[1]])
+            print("writing", file)
+            dihedrals = np.hstack([md.compute_phi(traj)[1], md.compute_psi(traj)[1]])
             self._save_and_assign_object(file, dihedrals)
         else:
-            print('not recalculating', file)
+            print("not recalculating", file)
 
-        file = self.files['features']
+        file = self.files["features"]
         if not file.exists() or recalculate:
-            print('writing', file)
+            print("writing", file)
             features = self.heavy_atom_distances(traj, periodic=periodic)
             self._save_and_assign_object(file, features)
         else:
-            print('not recalculating', file)
+            print("not recalculating", file)
+
+    def _heavy_atom_indices(self, traj):
+        return traj.topology.select("resname != HOH && type != H")
 
     def heavy_atom_distances(self, traj, periodic=True):
         """Return : ndarray with shape (num_frames, num_pairs)
-            The 45 atom-pair distances for an mdtraj Trajectory
+        The 45 atom-pair distances for an mdtraj Trajectory
         """
-        heavy_atoms = traj.topology.select('resname != HOH && type != H')
-        atom_pairs = np.array([(i, j)
-                               for i in heavy_atoms
-                               for j in heavy_atoms if i < j])
+        heavy_atoms = self._heavy_atom_indices(traj)
+        atom_pairs = np.array(
+            [(i, j) for i in heavy_atoms for j in heavy_atoms if i < j]
+        )
         return md.compute_distances(traj, atom_pairs, periodic=periodic)
 
+    def heavy_atom_positions(self, traj):
+        """Save heavy-atom Cartesian coordinates.
+
+        Parameters
+        ----------
+        traj : mdtraj.Trajectory
+
+        Saves
+        -----
+        heavy_atoms.npy : ndarray, shape (num_heavy_atoms * 3, num_frames)
+            Heavy-atom xyz coordinates, one column per frame.
+        """
+        heavy_atoms = self._heavy_atom_indices(traj)
+        xyz = traj.xyz[:, heavy_atoms, :]
+        positions = xyz.reshape(xyz.shape[0], -1).T
+        self.save_and_assign_objects({"heavy_atoms": positions})
+        return positions
+
     def save_eigen_data(self, srv, features=None, num_cvs=2):
-        """Transform/save data from a single simulation.
-            * call once SRV has been fitted
-            * SRV not intended to be used after this (sends srv.net to CPU)
+        """Transform/save vampnet eigenfunction data from a single simulation.
+        * call once SRV has been fitted
+        * SRV not intended to be used after this (sends srv.net to CPU)
         """
         if features is None:
             features = self.features
         psi = srv(features)
 
+        labels_objects = {
+            "srv": srv,
+            "eigvals": srv.eigvals,
+            "timescales": srv.timescales(),
+            "psi": psi,
+            "cvs": psi[:, :num_cvs],
+            "srv_net": srv.srv_net(),  # sends srv.net to the CPU
+        }
+        srv.net.to(device=srv.device)  # put it back
+        self.save_and_assign_objects(labels_objects)
+
+    def save_grid_data(self, srv):
+        """Project eigenfunctions onto the dihedral grid.
+
+        Call after save_eigen_data (or with an SRV whose net is already on
+        srv.device, as save_eigen_data leaves it).
+        """
         feature_grid = feature_grid_over_dihedrals(
             self.features, self.dihedrals, self.theta_grid
         )
         psi_grid = psi_grid_from_feature_grid(feature_grid, srv)
         cvs_grid = cvs_grid_from_feature_grid(feature_grid, srv)
 
-        labels_objects = {
-            'srv': srv,
-            'eigvals': srv.eigvals,
-            'timescales': srv.timescales(),
-            'psi': psi,
-            'cvs': psi[:, :num_cvs],
-            'feature_grid': feature_grid,
-            'psi_grid': psi_grid,
-            'cvs_grid': cvs_grid,
-            'srv_net': srv.srv_net(), # sends srv.net to the CPU
-        }
-        srv.net.to(device=srv.device) # put it back
-        self.save_and_assign_objects(labels_objects)
-
-    def save_kde_fes_bias_data(self, p, s1, s2, widths, kde, kde_c):
-        """Saves data in variables:
-
-        s1 : array, shape (len_s1,)
-        s2 : array, shape (len_s1,)
-        widths : array, shape (2,)
-
-        kde : KDE          Uncompressed, normalized
-        kde_c : KDE        Compresssed & normalized
-
-        grid : dict
-            'hist' : The histogram of P(s)
-            'kde' : The (compressed) KDE estimate for P(s)
-
-        fes : dict
-            'hist' : F(s) computed using histogram P(s)
-            'kde' : F(s) computed using KDE P(s)
-
-        bias : dict
-            'bp' : w(s) computed using opes.BiasPotential
-            'fm' : w(s) computed using force.ForceModule
-            'fes' : F(s) computed using w(s) from BiasPotential
-        """
-        self.save_and_assign_objects({'s1': s1, 's2': s2, 'widths': widths,
-                                      'kde': kde, 'kde_c': kde_c})
-
-        temperature = p.temperature
-        epsilon = p.dist_regularization
-        bias_factor = p.bias_factor
-
-        grid = {
-            'hist': histogram2d(s1, s2, self.cvs),
-            'kde': calc_kde_grid(s1, s2, kde_c)
-        }
-        fes = {
-            'hist': free_energy_surface(grid['hist'], temperature, epsilon),
-            'kde': free_energy_surface(grid['kde'], temperature, epsilon)
-        }
-        self.save_and_assign_objects({'grid': grid,  'fes': fes})
-
-        bp = bias_potential(p, kde_c)
-        fm = force_module(bp, kde_c, self.srv_net.cpu())
-        gr = calc_bias_grid(s1, s2, bp)
-        bias = {
-            'bp': gr,
-            'fm': calc_bias_grid_fm(s1, s2, fm),
-            'fes': calc_fes_from_bias(gr, bias_factor)
-        }
-        self.save_and_assign_objects({'bias': bias})
-
-    # def save_kde_fes_data(self, s1, s2, widths, kde, kde_c, temperature, epsilon):
-    #     from .hist2d import histogram2d
-    #     from .calcs import calc_kde_grid, free_energy_surface
-
-    #     grid = {
-    #         'hist': histogram2d(s1, s2, self.cvs),
-    #         'kde': calc_kde_grid(s1, s2, kde_c)
-    #     }
-    #     fes = {
-    #         'hist': free_energy_surface(grid['hist'], temperature, epsilon),
-    #         'kde': free_energy_surface(grid['kde'], temperature, epsilon)
-    #     }
-    #     labels_objects = {
-    #         's1': s1, 's2': s2, 'widths': widths,
-    #         'kde': kde, 'kde_c': kde_c,
-    #         'grid': grid, 'fes': fes,
-    #     }
-    #     self.save_and_assign_objects(labels_objects)
-
-    # def dataset_srv_data(self, srv, collected_data, num_cvs=2, pad=0.07):
-    #     """Replacing 'save_eigen_data': call once SRV has been fitted
-
-    #     Save:
-    #         eigvals.npy
-    #         timescales.npy
-
-    #         dihedrals_full.npy  # --- full dataset ---
-    #         weights_full.npy
-    #         psi.npy
-    #         cvs.npy
-
-    #         srv.pickle          # --- objects ---
-    #         srv_net.pt
-
-    #     Parameters
-    #     ----------
-    #     srv : vampnet.SRV
-    #     collected_data : dict(list(np.ndarray))
-    #         Data collection from '.util.prepare_data'
-    #     pad : float : for 'ranges_data'
-
-    #     Return
-    #     ------
-    #     ranges : list(list(float))
-    #         Full set of ranges from psi.npy
-    #     cvs_list : list(np.ndarray)
-    #         List of transformed data (CVs) for plotting
-    #     """
-    #     from .hist2d import ranges_data
-
-    #     # transform feature data and be done with SRV
-    #     # - srv.net will be sent to cpu in srv.srv_net()
-    #     psi_list = [srv(x) for x in collected_data['features']]
-    #     cvs_list = [psi[:, :num_cvs] for psi in psi_list]
-    #     psi = np.vstack(psi_list)
-
-    #     labels_objects = {
-    #         'srv': srv,
-    #         'eigvals': srv.eigvals,
-    #         'timescales': srv.timescales(),
-    #         'psi': psi,
-    #         'cvs': psi[:, :num_cvs],
-    #         'srv_net': srv.srv_net(),
-    #         'dihedrals_full': np.vstack(collected_data['dihedrals']),
-    #         'weights_full': np.hstack(collected_data['weights']),
-    #     }
-    #     self.save_and_assign_objects(labels_objects)
-    #     return ranges_data(psi, pad=pad), cvs_list, psi_list
-
-
-# def symlink_to_sd_labels(sd, labels, dst_dir):
-#     """Create a symlink in 'dst_dir' pointing to each file [sd.files[label] for label in labels]
-#     """
-#     if isinstance(dst_dir, str):
-#         dst_dir = Path(dst_dir)
-#     elif not isinstance(dst_dir, Path):
-#         raise TypeError(f'dst_dir is unrecognized type {type(dst_dir)}')
-
-#     for label in labels:
-#         src = Path.cwd() / sd.files[label] # src must be absolute path
-#         dst = dst_dir / src.name          # dst must include file name
-#         if dst.exists():
-#             print(f'File {dst} exists.  Not creating new symlink.')
-#         else:
-#             dst.symlink_to(src)
+        self.save_and_assign_objects(
+            {
+                "feature_grid": feature_grid,
+                "psi_grid": psi_grid,
+                "cvs_grid": cvs_grid,
+            }
+        )
